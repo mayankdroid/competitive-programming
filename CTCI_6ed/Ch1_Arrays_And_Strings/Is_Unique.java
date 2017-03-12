6b45220249f742fd3996900731b98fd4477d13a2
Wilson Ding <shodai100@gmail.com>
2017-03-11 23:08:46 -0600

    Added Brute Force solution to Is_Unique.java

---
+++ CTCI_6ed/Ch1_Arrays_And_Strings/Is_Unique.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+class Is_Unique {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String inputString = scan.nextLine();
+
+		boolean isUnique = true;
+
+		if(inputString.length() > 26) { // only 26 unique characters
+			isUnique = false;
+		} else {
+			for(int i = 0; i < inputString.length(); i++) {
+				for(int j = i + 1; j < inputString.length(); j++) {
+					if(inputString.charAt(i) == inputString.charAt(j)) { // Brute force through every possible combination
+						isUnique = false;
+					}
+				}
+			}
+		}
+
+		if(isUnique) {
+			System.out.println("The string has all unique characters.");
+		} else {
+			System.out.println("The string has duplicates.");
+		}
+	}
+}
