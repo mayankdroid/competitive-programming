4745dbeae086e718d64d681150a6a7c7f29b9c3a
Wilson Ding <shodai100@gmail.com>
2017-03-24 00:28:18 -0500

    Refactored solution 007 for Project Euler

--- ProjectEuler/007_10001st_Prime.java
+++ ProjectEuler/007_10001st_Prime.java
@@ -6,29 +6,34 @@ What is the 10 001st prime number?
 
 import java.io.*;
 import java.util.*;
+import java.math.*;
 
 class TenThousandAndOne_Prime {
+	public static boolean isPrime(long value) {
+		boolean isPrime = true;
+
+		for(int i = 2; i < Math.ceil(value/2); i++) {	// iterate from 2....Math.ceil(value/2)
+			if(value % i == 0) {	// if %i == 0; we have another divisor == not prime
+				isPrime = false;
+				break;
+			}
+		}
+
+		return isPrime;
+	}
+
 	public static void main(String[] args) {
 		int numOfPrimes = 0;	// counting primes
 		long currentValue = 1;	// current value we're checking for primes
 
-		while(numOfPrimes < 10001) {	// iterate until we find our 10001'th prime
-			boolean isPrime = true;
-
+		while(numOfPrimes <= 10001) {	// iterate until we find our 10001'th prime
 			currentValue++;
 
-			for(int i = 2; i < currentValue; i++) {	// iterate from 2....currentValue-1
-				if(currentValue%i == 0) {	// if %i == 0; we have another divisor == not prime
-					isPrime = false;
-					break;
-				}
-			}
-
-			if(isPrime) {	// if we find a prime number, add 1 to count of primes
+			if(isPrime(currentValue)) {	// if we find a prime number, add 1 to count of primes
 				numOfPrimes++;
 			}
 		}
 
-		System.out.println(currentValue);
+		System.out.println("The 10,001st prime number is: " + currentValue);
 	}
 }
