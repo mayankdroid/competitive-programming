078388eed9db71a63ba6d95326bff607f3ec98c4
Wilson Ding <shodai100@gmail.com>
2017-04-05 09:13:17 -0500

    Added template for coding competitions

---
+++ Misc/Template.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+class Template {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+
+		int value = scan.nextInt();
+	}
+}
